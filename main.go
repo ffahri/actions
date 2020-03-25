20059c3e429d6cab3844b178ada09f9ee9555036
Fahri Yardımcı <f.yardimci06@gmail.com>
2020-03-25 14:32:21 +0300

    changed lambda code and zip deploy

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func HandleRequest(ctx context.Context) (string, error) {
-	return fmt.Sprintf("Hello world"), nil
+	return fmt.Sprintf("Hello msg: " + os.Getenv("message")), nil
 }
 
 func main() {
